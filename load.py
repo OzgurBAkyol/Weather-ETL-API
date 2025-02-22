76e4245f3fb07a980a77d702ad5c5738d3c8fdd5
ozgurberkeakyol <ozgurba99@gmail.com>
2025-02-22 19:28:08 +0300

    v2

---
+++ load.py
@@ -0,0 +1,34 @@
+import psycopg
+import pandas as pd
+
+conn = psycopg.connect(
+    dbname="your_dbname",
+    user="your_username",
+    password="your_password",
+    host="your_host",
+    port="your_port"
+)
+
+#df = pd.read_csv("weather_data.csv")
+
+with conn.cursor() as cur:
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS weather_data (
+            time TIMESTAMP,
+            temperature FLOAT,
+            wind_speed FLOAT
+        )
+    """)
+
+    for index, row in df.iterrows():
+        cur.execute("""
+            INSERT INTO weather_data (time, temperature, wind_speed)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (time) DO UPDATE SET
+                temperature = EXCLUDED.temperature,
+                wind_speed = EXCLUDED.wind_speed
+        """, (row['time'], row['temperature'], row['wind_speed']))
+
+    conn.commit()
+
+conn.close()
